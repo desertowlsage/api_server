f5a5ff7ae8c651c9d6e83419896161d02e0e0cbf
desertowl.sage <desertowl.sage@gmail.com>
2024-03-24 15:23:47 +0530

    add new endpoint

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/hello-world", handleHelloWorld)
 	http.HandleFunc("/health", handleHealth)
+	http.HandleFunc("/new-endpoint", handleNewEndpoint)
 
 	addr := "localhost:8000"
 	log.Printf("Listening on %s ...", addr)
@@ -27,6 +28,13 @@ func handleHelloWorld(writer http.ResponseWriter, request *http.Request) {
 }
 
 func handleHealth(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+	writeRespose(writer, "Ok!")
+}
+
+func handleNewEndpoint(writer http.ResponseWriter, request *http.Request) {
 	if request.Method != "GET" {
 		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 	}
